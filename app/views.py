0c2de9f4b0f6297186a8eaaddc038950f51eb024
himasaila8336 <himasaila8336@gmail.com>
2023-03-11 21:06:01 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def hima(request):
+    return render(request,'hima.html')
+def sailu(request):
+    return render(request,'sailu.html')    
